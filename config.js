020b31a1523b273355b48825b9a62ba4bdaba337
afreakinbed <130297111+afreakinbed@users.noreply.github.com>
2023-04-09 10:23:28 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     token: "MTAxNzkyNDE0OTkyODI4MDA5NA.GHrYut.7LJMM-b6z64yx7bYTlEXvY1vCuFgDa_hEW4YbA", // place your token here (no token = QR Login)
-    password: "Lolirock0+", // The password is mandatory to change the nickname & tag
-    username: "! Sans", // The username to snipe (no username === no work)
-    tag: "0527" // The tag to snipe (no tag === no work)
+    password: "georgefloyd111", // The password is mandatory to change the nickname & tag
+    username: "niger", // The username to snipe (no username === no work)
+    tag: "#0001" // The tag to snipe (no tag === no work)
 }
